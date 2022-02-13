acaf0a1197e8a8ad266c2ef9b9642b0104989a1e
MacRat <m@crat.jp>
2022-02-13 13:26:49 +0900

    write package document

--- __init__.py
+++ __init__.py
@@ -1,4 +1,23 @@
-""" wordpy -- word guess game.
+""" Word guess game.
+
+>>> class MySolver(wordpy.Solver):
+...     def __init__(self, game: wordpy.Game):
+...         super().__init__(game)
+...
+...         # do initialization here
+...
+...     def guess(self) -> wordpy.WordTable:
+...         self.state # current game status.
+...         self.words # current word candidates.
+...
+...         self.drop_words_by_state() # drop words that can't be the answer, from self.words.
+...
+...         return self.words[:10] # return some next word candidates.
+...
+
+
+>>> words = wordpy.get_words()
+>>> wordpy.benchmark(words, MySolver)
 """
 
 from .game import Game, FixedGame, TerminalGame
